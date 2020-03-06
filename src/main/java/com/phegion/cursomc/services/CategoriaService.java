b825f1a60dcefa20e35fa3f74a757ff2c3826592
Phegion <marcoantoniocastilholeite@gmail.com>
2020-03-06 07:42:10 -0300

    Inserindo nova categoria com POST, e atualizando com PUT

--- src/main/java/com/phegion/cursomc/services/CategoriaService.java
+++ src/main/java/com/phegion/cursomc/services/CategoriaService.java
@@ -11,13 +11,23 @@ import com.phegion.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
-	@Autowired
-	private CategoriaRepository repo;	
 
-	public Categoria find(Integer id) {  //BUSCAR CATEGORIA PELO ID
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(   //EXCEPTION CASO O ID SEJA INEXISTENTE
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
 	}
 }
