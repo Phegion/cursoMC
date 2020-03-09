e3e64f70aa14e94968e8469aadf25248a5cfa0b9
Phegion <marcoantoniocastilholeite@gmail.com>
2020-03-09 10:24:32 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/phegion/cursomc/services/ClienteService.java
+++ src/main/java/com/phegion/cursomc/services/ClienteService.java
@@ -1,23 +1,64 @@
 package com.phegion.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.phegion.cursomc.domain.Cliente;
+import com.phegion.cursomc.dto.ClienteDTO;
 import com.phegion.cursomc.repositories.ClienteRepository;
+import com.phegion.cursomc.services.exception.DataIntegrityException;
 import com.phegion.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
-	
-	@Autowired
-	private ClienteRepository repo;	
 
-	public Cliente find(Integer id) {  //BUSCAR CLIENTE PELO ID
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente find(Integer id) { // BUSCAR CLIENTE PELO ID
 		Optional<Cliente> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(   //EXCEPTION CASO O ID SEJA INEXISTENTE
+		return obj.orElseThrow(() -> new ObjectNotFoundException( // EXCEPTION CASO O ID SEJA INEXISTENTE
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) { // METODO AUXILIAR PARA CONVERTER UM OBJ DTO INSTANCIAR UMA ENTITY
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
 }
