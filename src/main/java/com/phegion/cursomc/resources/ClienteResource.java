b0303fb638ef6cf3fe330f8139bafe4d0f67271d
Phegion <marcoantoniocastilholeite@gmail.com>
2020-03-12 08:23:59 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/phegion/cursomc/resources/ClienteResource.java
+++ src/main/java/com/phegion/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.phegion.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.phegion.cursomc.domain.Cliente;
 import com.phegion.cursomc.dto.ClienteDTO;
+import com.phegion.cursomc.dto.ClienteNewDTO;
 import com.phegion.cursomc.services.ClienteService;
 
 @RestController
@@ -25,13 +28,22 @@ public class ClienteResource {
 	
 	@Autowired
 	private ClienteService service;
-
+	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
-
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
 		Cliente obj = service.fromDTO(objDto);
@@ -62,5 +74,5 @@ public class ClienteResource {
 		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));  
 		return ResponseEntity.ok().body(listDto);
-	}
-}	
+	}	
+}
