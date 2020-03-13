2422cc23d96fd5e0280b246078746cf8e7757f5f
Phegion <marcoantoniocastilholeite@gmail.com>
2020-03-13 08:10:29 -0300

    Validao customizada: email nao repetido na atualizacao de cliente

--- src/main/java/com/phegion/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/phegion/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.phegion.cursomc.domain.Cliente;
+import com.phegion.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
